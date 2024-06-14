c930ea8b598adf505052ecca3b752c67623b10f3
BeastWolf691 <sriver@hotmail.fr>
2024-06-14 13:23:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/projectstreamer/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.projectstreamer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.projectstreamer.ui.theme.ProjectStreamerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProjectStreamerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    Text(modifier = Modifier.padding(start = 50.dp, top = 75.dp)) {
+        text = "entre copain copine $name!",
+        modifier = modifier
+    }
+    Column(modifier = Modifier.padding(start = 25.dp, top = 75.dp)) {
+        Icon(Icons.Filled.Create, contentDescription = "")
+
+    }
+
+}
+
+
