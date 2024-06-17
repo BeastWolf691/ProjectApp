66a7cbdf23a6c8ee9f09afae3735249bff4d26ea
Steven Riviere <sriver@hotmail.fr>
2024-06-17 17:06:26 +0200

    update main and pictures

--- app/src/main/java/com/example/projectstreamer/MainActivity.kt
+++ app/src/main/java/com/example/projectstreamer/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,8 +16,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -25,8 +33,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -97,6 +107,7 @@ fun Header(
             Spacer(modifier = Modifier.width(30.dp)) // Ajoute un espace entre l'icône et la barre de recherche
 
             SearchBar(
+                modifier = Modifier.clip(RoundedCornerShape(100)),
                 query = "",
                 onQueryChange = {},
                 onSearch = {},
@@ -140,7 +151,86 @@ fun Footer(
 ) {
     val configuration = LocalConfiguration.current
     val screenHeight = configuration.screenHeightDp.dp
-    val desiredHeight = (screenHeight / 5) // 1/5s de la hauteur de l'écran
+    val desiredHeight = (screenHeight / 4) // 1/4 de la hauteur de l'écran
 
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(max = desiredHeight)
+            .background(Color.Gray) // Couleur de fond du footer
+            .padding(16.dp), // Ajustement du padding global du footer
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        // Liste de données pour les images et les phrases
+        val scrollState = rememberScrollState()
+        val imagesWithTexts = listOf(
+            ImageWithText(
+                imageResId = R.drawable.image1,//indique le chemin
+                text = "Star Wars"
+            ),
+            ImageWithText(
+                imageResId = R.drawable.image2,
+                text = "Stormtrooper"
+            ),
+            ImageWithText(
+                imageResId = R.drawable.image3,
+                text = "Je suis ton père"
+            ),
+            ImageWithText(
+                imageResId = R.drawable.image4,
+                text = "Ah Nan"
+            )
+        )
+
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            items(imagesWithTexts.chunked(4)) { rowItems ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .horizontalScroll(state = scrollState),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    rowItems.forEach { item ->
+                        ImageWithTextComponent(
+                            imageResId = item.imageResId,
+                            text = item.text
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
+
+fun items(chunked: List<List<ImageWithText>>, any: Any) {
+}
+
+// Modèle de données pour une image avec un texte associé
+data class ImageWithText(
+    val imageResId: Int,
+    val text: String
+)
+
+@Composable
+fun ImageWithTextComponent(imageResId: Int, text: String) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(horizontal = 8.dp)
+    ) {
+        Image(
+            painter = painterResource(imageResId),
+            contentDescription = null, // La description de contenu peut être null pour les images décoratives
+            modifier = Modifier.width(100.dp)// Taille de l'image
+        )
+        Text(
+            text = text,
+            fontSize = 16.sp, // Taille du texte
+            modifier = Modifier.padding(top = 2.dp) // Espace entre l'image et le texte
+        )
+    }
+}
+
 
